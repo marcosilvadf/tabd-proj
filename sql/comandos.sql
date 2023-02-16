d0223f62dc59098712122ce80b3c04e3c6a39cf9
marcosilvadf <marcosilva18contato@gmail.com>
2023-02-16 20:19:24 -0300

    aumento codigo

---
+++ sql/comandos.sql
@@ -0,0 +1,15 @@
+Selecionando o maior codigo:
+SELECT MAX(codigo) FROM `aluno` 
+
+Selecionando o menor codigo:
+SELECT MIN(codigo) FROM `aluno` 
+
+Conta a quantidade de codigo:
+SELECT COUNT(codigo) FROM `aluno` 
+
+Soma os campos codigo:
+SELECT SUM(codigo) FROM `aluno` 
+
+
+Mostra a media de codigo:
+SELECT SUM(codigo)/COUNT(codigo) FROM `aluno` 
