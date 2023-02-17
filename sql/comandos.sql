355c0a2a5c28b50593783f688d18c7a56540754d
marcosilvadf <marcosilva18contato@gmail.com>
2023-02-16 22:05:15 -0300

    1

--- sql/comandos.sql
+++ sql/comandos.sql
@@ -12,4 +12,10 @@ SELECT SUM(codigo) FROM `aluno`
 
 
 Mostra a media de codigo:
-SELECT SUM(codigo)/COUNT(codigo) FROM `aluno` 
+SELECT SUM(codigo)/COUNT(codigo) FROM `aluno`
+
+Mostra os alunos da disciplina
+SELECT a.codigo, a.nome, d.codigo, d.cod_aluno, d.nome_disciplina
+FROM aluno a, disciplina d
+WHERE a.codigo = d.cod_aluno AND a.nome LIKE 'a%'
+ORDER BY (a.nome) ASC;
